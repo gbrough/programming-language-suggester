5e3a498636db2ce4cb96307d7a454ddfc5d5e0d7
gbrough <gbrough@gmail.com>
2021-06-10 20:30:57 -0700

    change branch statement for new conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,14 @@
 //Business Logic
 function getLanguage(demand,purpose,size,industry,interest) {
   let survey = ''
-  if (demand === "learning") {
+  if (demand === "learning" && purpose === "front-end" && size === "small" && industry === "tech" && interest === "software") {
     survey = "JavaScript"
-  } else if (demand === "performance") {
-    survey = "C++"
-  } else if (purpose === "front-end") {
-    survey = "PHP"
-  } else if (purpose === "back-end") {
-    survey = "SQL"
-  } else if (size === "small") {
-    survey = "Ruby"
-  } else if (size === "large") {
-    survey = "C#"
-  } else if (industry === "tech") {
-    survey = "Swift"
-  } else if (industry === "other") {
-    survey = "Java"
-  } else if (interest === "software") {
+  } else if (demand === "performance" && purpose === "back-end" && size === "large" && industry === "other" && interest === "hardware") {
     survey = "Python"
-  } else if (interest === "hardware") {
-    survey = "Assembly"
-  } 
+  } else {
+    survey = "C++"
+  }
+
   return survey
 }
 //UI Logic
